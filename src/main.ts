ca087049c80a0333b60f4b1e5498049242fe49df
hamzamushtaq840 <hamzamushtaq840@gmail.com>
2026-05-25 02:37:11 +0500

    feat: add global validation pipe to enhance request validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //  Strips away rogue fields hackers try to inject
+      transform: true, //  Automatically converts raw JSON to real DTO class instances!
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
