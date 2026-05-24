16f9b31391cd84060b4b2b874df9cfd9040614f4
hamzamushtaq840 <hamzamushtaq840@gmail.com>
2026-05-25 03:50:11 +0500

    feat: implement AddonController and AddonService with CRUD operations

--- src/addon/addon.controller.ts
+++ src/addon/addon.controller.ts
@@ -1,7 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { AddonService } from './addon.service';
+import { CreateAddonDto } from './dto/create-addon.dto';
+import { GetAddonDto } from './dto/get-addon.dto';
 
-@Controller('addon')
+@Controller('addons')
 export class AddonController {
   constructor(private readonly addonService: AddonService) {}
+
+  @Get()
+  findAll() {
+    return this.addonService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param() params: GetAddonDto) {
+    return this.addonService.findOne(params);
+  }
+
+  @Post()
+  create(@Body() createAddonDto: CreateAddonDto) {
+    return this.addonService.create(createAddonDto);
+  }
+
+  @Delete(':id')
+  remove(@Param() params: GetAddonDto) {
+    return this.addonService.remove(params);
+  }
 }
--- src/addon/addon.service.ts
+++ src/addon/addon.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateAddonDto } from './dto/create-addon.dto';
+import { AddonParamsDto } from './dto/addon-params.dto';
+import { Addon } from './entity/addon.entity';
 
 @Injectable()
-export class AddonService {}
+export class AddonService {
+  constructor(
+    @InjectRepository(Addon)
+    private addonRep: Repository<Addon>,
+  ) {}
+
+  findAll() {
+    return this.addonRep.find();
+  }
+
+  findOne(params: AddonParamsDto) {
+    return this.addonRep.findOne({ where: { id: params.id } });
+  }
+
+  create(createAddonDto: CreateAddonDto): Promise<Addon> {
+    return this.addonRep.save(createAddonDto);
+  }
+
+  remove(params: AddonParamsDto) {
+    return this.addonRep.delete({ id: params.id });
+  }
+}
