d80f7354c32aef984d4394f157d459484b039380
hamzamushtaq840 <hamzamushtaq840@gmail.com>
2026-05-26 00:34:18 +0500

    feat: add global LoggerMiddleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AddonModule } from './addon/addon.module';
 import { DatabaseModule } from './database/database.module';
+import { LoggerMiddleware } from './common/logger.middleware';
 
 @Module({
   imports: [DatabaseModule, AddonModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
